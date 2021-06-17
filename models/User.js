c0348ddc53de4735531ea6317b5c4bddc536875e
Junyu_Han <johnny.jyhan@gmail.com>
2021-06-16 21:43:47 -0400

    updated user schema

--- models/User.js
+++ models/User.js
@@ -3,7 +3,14 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   firstName: String,
-  lastName: String
+  lastName: String,
+  addressOne: String,
+  addressTwo: String,
+  city: String,
+  state: String,
+  zip: String,
+  country: String,
+  timeStamp: Date
 })
 
 mongoose.model('users', userSchema);
